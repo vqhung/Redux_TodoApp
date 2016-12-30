129359ebd1575abcd9ee94cd5b1c9908180d0710
Hung Vo <vqhung@gmail.com>
2016-12-30 09:52:31 +0700

    Add todos reducer and test, update package json for babel-core to 6.10.4

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -28,4 +28,40 @@ describe('Reducers', () => {
       expect(res).toEqual(true);
     });
   });
+
+  describe('todoReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'walk the dog'
+      };
+
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toBe(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo', () => {
+      var todos = [{
+        id: '123',
+        text: 'todo test',
+        completed: true,
+        createdAt: 123,
+        completedAt: 456
+      }];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: '123'
+      };
+
+      var res = reducers.todosReducer(df(todos), df(action));
+
+      console.log(res);
+
+      expect(res[0].completed).toBe(false);
+      expect(res[0].compledtedAt).toBe(undefined);
+
+    });
+  });
 });
