b87aa5dcb14508d96dacd4ee29956935ee139013
Hung Vo <vqhung@gmail.com>
2016-12-30 10:17:20 +0700

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -3,7 +3,20 @@ var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var TodoApp = require('TodoApp');
 
+var actions = require('actions');
+var store = require('configureStore').configure();
 
+console.log(store);
+
+store.subscribe(() => {
+  console.log('New state ', store.getState());
+});
+
+store.dispatch(actions.addTodo('Cleans the yard'));
+
+store.dispatch(actions.setSearchText('year'));
+
+store.dispatch(actions.toggleShowCompleted());
 // Load foundation
 //require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
